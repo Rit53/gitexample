aedb23ef7290120394d327fb2a9bd886133e7ef5
rit53 <ritesh.kumar@liet.in>
2024-07-18 20:25:45 +0530

    some

---
+++ forin.js
@@ -0,0 +1,55 @@
+// let data=["red","green","blue"];
+// for(let x in data){               // for in loop use for array and object 
+//     console.log(x +":"+data[x])      
+// }
+
+// for(let x of data){               // for off loop only use for array
+//     console.log(x)      
+// }
+
+// data.map(function(data,index){     // map function
+//     console.log(index,data)
+// })
+
+
+// data.forEach(function(data,index){         // for each method 
+//         console.log(index,data)
+//     })
+
+// push pop shift unshift
+
+// data.pop()
+// data.push("orange")
+// data.shift()
+// data.unshift("red")
+
+// console.log(data)
+
+
+// let num=[22,23,24,25,26,27,28]
+// num.some((val)=>{                // some
+//     // if(val<=20){
+//         return val<=25;
+       
+//     })
+//     console.log(num)
+
+
+// num.every((val)=>{                // every 
+//     // if(val<=20){
+//         return val<=25;
+       
+//     })
+//     console.log(num)
+
+
+
+// array destructuring in javascript
+
+let color=["red","green","blue","orange"];
+
+const[a,b,c,d]=color;
+
+console.log(a,b,c,d);
+
+
